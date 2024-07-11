cb4c931423b87353e5eac69f4fbf4338d0e153fc
nemishv.tagline@.com <nemishv.tagline@gmail.com>
2024-07-11 12:40:08 +0530

    dependecy and edit remaining

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,30 +3,38 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def new
-    @user = User.new
-    @states = State.pluck(:name).sort()
-    # @cities = City.pluck(:name)
-  end
-
-  def create
-    @user = User.create(user_params)
-    if @user.save
-      render plain:'saved'
-    else
-      render :show,status: :unprocessable_entity
-    end
-  end
-  
   def show
     @user = User.find(params[:id])
   end
 
+  def new
+    @user = User.new
+    @states = State.pluck(:name).sort()
+    @state = State.select(:name).where(name: params[:state])
+    @cities = City.joins(:State).where(@state).pluck(:name)
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user, notice: 'User was successfully created.'
+    else
+      render :new,status: :unprocessable_entity
+    end
+  end
+  
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path
   end
 
   private
   def user_params
-    params.require(:users).permit(:username, :email, :gender, :state, :city, hobbies: [])
+    params.require(:user).permit(:username, :email, :gender, :state, :city, hobbies: [])
   end
 end
